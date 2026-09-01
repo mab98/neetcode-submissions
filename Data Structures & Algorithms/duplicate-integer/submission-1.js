5c6e01805571a17d118bb8c16e9ecb07f8d12c7c
Muhammad Abubakar <mhmdabubakar98@gmail.com>
2026-09-01 22:38:58 +0500

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const map = new Map()
+        for(let i = 0 ; i < nums.length ; i++){
+            if(map.get(nums[i])) return true
+            map.set(nums[i], 1)
+        }
+        return false
+    }
+}
